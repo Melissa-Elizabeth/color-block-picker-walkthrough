c1fa7f15f74c696ae5d7b6270804fde10df00fc3
Avatar <avatar@Avatars-MacBook-Pro.local>
2017-02-15 10:54:58 -0600

    color selection working

--- client.js
+++ client.js
@@ -8,8 +8,22 @@ for (var i = 0; i < colorArray.length; i++) {
   var newColorBlock = $('<div>'); // Empty div -Not added to page yet
   newColorBlock.css('background-color', colorArray[i]); // Adds color to empty div (the ith color)
   newColorBlock.addClass('colorBlock'); // Adds class to empty div - gives height and width
+  newColorBlock.data('colorOfBlock', colorArray[i]); // Adds information to DOM without showing it to the user
   $('#colorBlockContainer').append(newColorBlock); // Add the div to the DOM
 }
+
+$('#colorBlockContainer').on('click', '.colorBlock', function(){ // event listener
+  console.log('colorBlock was clicked');
+  console.log('$(this : ', $(this));
+  console.log('$(this).data() : ', $(this).data());
+  console.log('$(this).data().colorOfBlock : ', $(this).data().colorOfBlock);
+  var colorOfBlockSelected = $(this).data().colorOfBlock;
+  if(randomColor == colorOfBlockSelected) {
+ $('#responseSection').text('You got it!');
+  } else {
+    $('#responseSection').text('Oh no. That is not right');
+  }
+});
 
 // Select a random color and add it to the DOM
 var randomNumberSelected = randomNumber(0, colorArray.length - 1); // get random number from 0 through 4 and assigns that to randomNumberSelected
